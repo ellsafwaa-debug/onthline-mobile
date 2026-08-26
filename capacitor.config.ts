d8fdad0e01468f12e522c952e2226eee100260b2
ellsafwaa-debug <ellsafwaa@gmail.com>
2026-08-27 02:34:25 +0300

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.onthline.app',
-  appName: 'ONTHLINE',
-  webDir: 'www'
-};
-
-export default config;
